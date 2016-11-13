c7bc4f6d2f4c97fd56839f127cfbbdd15dea82a1
Jeff Kluge <jeffkl@microsoft.com>
2016-11-13 15:27:06 -0800

    Fix NUnit tests which weren't running

---
+++ src/CSharp/Windows/UnitTests.NUnit/NUnitExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnit
+{
+    [TestFixture]
+    public class NUnitExample
+    {
+        [Test]
+        public void TestThatPasses()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}

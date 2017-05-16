69d5200b06e302637cd35f4b065f9c7c3855f503
Jeff Kluge <jeffkl@microsoft.com>
2017-05-16 12:58:00 -0700

    Updates (#7)
    * remove log4net
    
    * Temporarily pin NuGet 3.4
    
    * Upgrade CBT
    
    * Example for use of global packages

---
+++ src/Modules/NuGet/UseGlobalPackages/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace UseGlobalPackages
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            JObject jo = new JObject();
+        }
+    }
+}
---
+++ src/Modules/NuGet/UseGlobalPackages/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("UseGlobalPackages")]
+[assembly: AssemblyDescription("")]
